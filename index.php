056a0ee348abf1b801b5aca0f971d3d61232143c
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2020-12-22 16:24:07 +0500

    create_templates

--- index.php
+++ index.php
@@ -3,19 +3,8 @@ session_start();
 if ($_SESSION['user']) {
     header("Location: profile.php");
 }
+require('./templates/head.php');
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="utf8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="/message_wall/styles/styles.css" rel="stylesheet">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <title>Стена сообщений</title>
-</head>
-
-
 <body>
     <div class="container">
         <form class="form-signin" method="POST" action="/message_wall/vendor/signin.php">
