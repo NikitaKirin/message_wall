9bbd69419496355fe7f304cb9b8b65340b5bd77d
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2020-12-22 16:29:25 +0500

    add_template_for_register.php

--- register.php
+++ register.php
@@ -3,16 +3,8 @@ session_start();
 if ($_SESSION['user']) {
     header("Location: profile.php");
 }
+require('./templates/head.php');
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="utf8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="/message_wall/styles/styles.css" rel="stylesheet">
-    <title>Стена сообщений</title>
-</head>
 
 <body>
     <div class="container">
